ca91ef4e84fb825f84e39a955e6615233daa632e
SimoJOUDAR <ssjoudar@gmail.com>
2022-04-25 16:26:55 +0200

    Empty Activity project

---
+++ app/src/main/java/fr/joudar/mareu/ui/MeetingsListActivity.java
@@ -0,0 +1,16 @@
+package fr.joudar.mareu.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.joudar.mareu.R;
+
+public class MeetingsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meetings_list);
+    }
+}

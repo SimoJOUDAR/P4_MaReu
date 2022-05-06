ffa8eeddce4e2f05e976c757e72b5a3ce9977ad3
SimoJOUDAR <ssjoudar@gmail.com>
2022-05-06 10:48:52 +0200

    AddMeetingActivity AutoCompleteTextView adapter config

--- app/src/main/java/fr/joudar/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/fr/joudar/mareu/ui/AddMeetingActivity.java
@@ -1,18 +1,27 @@
 package fr.joudar.mareu.ui;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
+import java.util.List;
+
 import fr.joudar.mareu.R;
 import fr.joudar.mareu.databinding.ActivityAddMeetingBinding;
+import fr.joudar.mareu.di.DI;
+import fr.joudar.mareu.model.Room;
 
 public class AddMeetingActivity extends AppCompatActivity {
 
     ActivityAddMeetingBinding binding;
 
+    Room mRoom;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +30,10 @@ public class AddMeetingActivity extends AppCompatActivity {
         setContentView(view);
         setSupportActionBar(binding.newMeetingToolbar);
 
+        initRoomsListSpinner();
+
         this.createNewMeeting();
+
     }
 
     private void createNewMeeting(){
@@ -34,5 +46,14 @@ public class AddMeetingActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_create_meeting, menu);
         return true;
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initRoomsListSpinner(){
+        List<Room> roomsList = DI.getApiService().getRoomsList();
+        RoomsListSpinnerAdapter roomsAdapter = new RoomsListSpinnerAdapter(getApplicationContext(), R.layout.rooms_list_spinner_item, roomsList);
+        binding.roomAutoCompleteTextView.setAdapter(roomsAdapter);
+        binding.roomAutoCompleteTextView.setOnItemClickListener(((adapterView, view, i, l) ->
+                mRoom = roomsAdapter.getItem(i)));
     }
 }
---
+++ app/src/main/java/fr/joudar/mareu/ui/RoomsListSpinnerAdapter.java
@@ -0,0 +1,56 @@
+package fr.joudar.mareu.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import fr.joudar.mareu.R;
+import fr.joudar.mareu.databinding.RoomsListSpinnerItemBinding;
+import fr.joudar.mareu.model.Room;
+
+public class RoomsListSpinnerAdapter extends ArrayAdapter<Room> {
+
+    public RoomsListSpinnerAdapter(@NonNull Context context, int resource, @NonNull List<Room> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initCustomView(position, convertView, parent);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return initCustomView(position, convertView, parent);
+    }
+
+    @NonNull
+    private View initCustomView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+//        RoomsListSpinnerItemBinding binding = RoomsListSpinnerItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+//        Room room = (Room) getItem(position);
+//        binding.roomsListSpinnerItemIcon.setImageResource(room.getRoomIcon());
+//        binding.roomsListSpinnerItemName.setText(room.getRoomName());
+//        return binding.getRoot();
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.rooms_list_spinner_item, parent, false);
+        }
+        ImageView icon = convertView.findViewById(R.id.rooms_list_spinner_item_icon);
+        TextView name = convertView.findViewById(R.id.rooms_list_spinner_item_name);
+        Room room = getItem(position);
+        icon.setImageResource(room.getRoomIcon());
+        name.setText(room.getRoomName());
+        return convertView;
+
+    }
+}
